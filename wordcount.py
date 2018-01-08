b86ffd81d000c5219a97c685d7e042fd79a722ab
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 12:48:13 -0800

    changed print method to iteritems() + added context manager

--- wordcount.py
+++ wordcount.py
@@ -6,19 +6,23 @@
 # do the .get method demonstrated in lecture
 
 def word_counter(file_path):
-    file = open(file_path)
+    # text_file = open(file_path)
 
-    counted_words = {}
-    for line in file:
-        line = line.rstrip()
-        word_list = line.split()
+    with open(file_path) as text_file:  # this is a context manager, this implementation is auto-close
 
-        for word in word_list:
-            counted_words[word] = counted_words.get(word, 0) + 1
+        counted_words = {}
+        for line in text_file:
+            line = line.rstrip()
+            word_list = line.split()
+
+            for word in word_list:
+                counted_words[word] = counted_words.get(word, 0) + 1
+
     return counted_words
 
-    file.close()
+    # text_file.close()
 
 result = word_counter("twain.txt")
-for word, count in result.items():
-    print "{} {}".format(word, count)
+for word, count in result.iteritems():
+    print "{} {}" .format(word, count)
+
